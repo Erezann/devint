f0136944f7fc18a221caaedc7019b0d4350c0e85
Amir BEN SLIMANE <benslima@polytech.unice.fr>
2014-02-26 15:46:00 +0100

    Super classe Button de la mort qui tue

---
+++ src/View/Button.java
@@ -0,0 +1,39 @@
+package view;
+
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JButton;
+
+import config.Config;
+
+public class Button extends JButton {
+	private String title;
+	
+	public Button(String title){
+		super(title);
+		this.setTitle(title);
+		init();
+	}
+
+	private void init() {
+		this.setFont(new Font(Config.FONT_BUTTON, Font.CENTER_BASELINE,
+				Config.FONT_SIZE_BUTTON));
+		this.setSize(new Dimension(100,40));
+		this.setForeground(Config.FONT_COLOR_BUTTON);
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
